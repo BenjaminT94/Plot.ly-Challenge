52c31a946aa7e1aadcb70586178300a631b3f0d3
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-16 16:55:28 -0700

    App.js modification

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-function buildMetadata(sample) {
+function Metadata(sample) {
     d3.json("samples.json").then((data) => {
       var metadata= data.metadata;
       var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
@@ -14,19 +14,16 @@ function buildMetadata(sample) {
   }
 
 
-function buildCharts(sample) {
+function VariousCharts(sample) {
 
 
   d3.json("samples.json").then((data) => {
     var samples= data.samples;
     var resultsarray= samples.filter(sampleobject => sampleobject.id == sample);
     var result= resultsarray[0]
-
     var ids = result.otu_ids;
     var labels = result.otu_labels;
     var values = result.sample_values;
-
-
     // Build the Bubble Chart
     var LayoutBubble = {
       margin: { t: 0 },
