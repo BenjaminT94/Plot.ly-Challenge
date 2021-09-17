4692a9d8aab37c9ba0a47e3d738996561e1f3fcc
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-16 23:52:47 -0700

    saved

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,20 +3,18 @@ function Metadata(sample) {
       var metadata= data.metadata;
     //   Utilizing the arrow method to call on the filter function
     // Use sample_values as the values for the bar chart
-      var array= metadata.filter(sampleobject => sampleobject.id == sample);
+      var array= metadata.filter(samplevalue => samplevalue.id == sample);
       var result= array[0]
-      var PANEL = d3.select("#sample-metadata");
-      PANEL.html("");
+      var selectsample = d3.select("#sample-metadata");
+      selectsample.html("");
       Object.entries(result).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${key}: ${value}`);
+        selectsample.append("h6").text(`${key}: ${value}`);
       });
     });
   }
 
 
 function VariousCharts(sample) {
-
-
   d3.json("samples.json").then((data) => {
     var samples= data.samples;
     var array= samples.filter(sampleobject => sampleobject.id == sample);
@@ -33,12 +31,16 @@ function VariousCharts(sample) {
 
       var Data = [
       {
+        // Use otu_ids for the x values
         x: ids,
+        // Use sample_values for the y values, in this case the values are already filtered from sample
         y: values,
         text: labels,
         mode: "markers",
         marker: {
+          // Use otu_ids for the marker colors
           color: ids,
+          // Use sample_values for the marker size
           size: values,
           }
       }
@@ -50,6 +52,7 @@ function VariousCharts(sample) {
     
     var bar_data =[
       {
+        // Use otu_ids as the labels for the bar chart
         y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
         x:values.slice(0,10).reverse(),
         text:labels.slice(0,10).reverse(),
@@ -71,22 +74,22 @@ function VariousCharts(sample) {
  
 function init() {
   // Creating the dropdown element
-  var selector = d3.select("#selDataset");
+  var menu = d3.select("#selDataset");
 
   // Creating the select options
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
-      selector
+      menu
         .append("option")
         .text(sample)
         .property("value", sample);
     });
 
     // Building the initial plots
-    const firstSample = sampleNames[0];
-    VariousCharts(firstSample);
-    Metadata(firstSample);
+    const Sample = sampleNames[0];
+    VariousCharts(Sample);
+    Metadata(Sample);
   });
 }
 
