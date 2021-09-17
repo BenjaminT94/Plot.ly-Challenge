6b9a93df466899d4678a94af4967427a8367704d
Benjamin Tran <Benjietran1994@gmail.com>
2021-09-17 13:09:40 -0700

    more code changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -19,13 +19,12 @@ function VariousCharts(sample) {
     var samples= data.samples;
     var array= samples.filter(sampleobject => sampleobject.id == sample);
     var result= array[0]
-    var ids = result.otu_ids;
     var labels = result.otu_labels;
     var values = result.sample_values;
+    var ids = result.otu_ids;
     // Build the Bubble Chart with Plotly
     var Layout = {
-      margin: { t: 0 },
-      xaxis: { title: "Id's" },
+      xaxis: { title: "OTU ID" },
       hovermode: "closest",
       };
 
